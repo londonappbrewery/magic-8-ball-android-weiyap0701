5acc4157568514d1c897595be4f7dd4e7f229985
weiyap <ongweiyap@gmail.com>
2017-09-23 17:36:07 +0800

    Done

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,16 @@
 package com.londonappbrewery.magiceightball;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+import static com.londonappbrewery.magiceightball.R.id.selectButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupEverything();
+    }
+
+    void setupEverything()
+    {
+        Button myButton = (Button) findViewById(R.id.selectButton);
+        final ImageView selectionImageView = (ImageView) findViewById(R.id.selectionImageView);
+        final int[] myArray = {R.drawable.ball1,R.drawable.ball2,R.drawable.ball3,R.drawable.ball4,R.drawable.ball5};
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random ranNumberGenerator = new Random();
+                final int ranNumber = ranNumberGenerator.nextInt(5);
+                selectionImageView.setImageResource(myArray[ranNumber]);
+                Log.d("Button click","fgfgf");
+            }
+        });
     }
 }
